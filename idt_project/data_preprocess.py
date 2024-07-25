86484f2df496b8808a8f4f79fdd5542472744a5c
Chaoyi He <chaoyi_he@tamu.edu>
2024-07-25 17:03:28 -0500

    modified:   .gitignore 	modified:   idt_project/data_preprocess.py

--- idt_project/data_preprocess.py
+++ idt_project/data_preprocess.py
@@ -4,5 +4,23 @@ import pandas as pd
 import os
 
 
-def get_raw_data():
+def get_raw_data(file_path):
+    """
+    Read the raw data from the file
+    :param file_path: the path of the file
+    :return: the raw data
+    """
+    customized_loader = LoadDataset()
+    channel_obj = ChannelIndSpectrogram()
     
+    snr_range = np.arange(40,80)
+    
+    assert os.path.isfile(file_path), "The file does not exist."
+    data, label = customized_loader.load_iq_samples(file_path, 
+                                                    dev_range=np.arange(0, 30, dtype=int),
+                                                    pkt_range=np.arange(0, 500, dtype=int))
+    data = awgn(data, snr_range)
+    data = channel_obj.channel_ind_spectrogram(data)
+    
+    return data, label
+
